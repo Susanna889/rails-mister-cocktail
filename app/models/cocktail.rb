3348e30d2726432673dfc5bd8370127cf124b5b6
Susanne Martin <susannemartin@gmx.net>
2019-11-21 12:29:14 +0200

    Mr Cocktail

---
+++ app/models/cocktail.rb
@@ -0,0 +1,8 @@
+class Cocktail < ApplicationRecord
+  has_many :ingredients, through: :doses
+  has_many :doses, dependent: :destroy
+  has_many :reviews
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+end
